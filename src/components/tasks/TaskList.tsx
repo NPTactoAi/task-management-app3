b52a18288d1ef3ad933fe279a9fe6756bbc3eab1
NPTactoAi <niklas.plueckebaum@tacto.ai>
2025-02-22 11:22:20 +0100

    Add TaskList component

---
+++ src/components/tasks/TaskList.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function TaskList() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Task List</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p>Your tasks will appear here</p>
+      </div>
+    </div>
+  );
+}
